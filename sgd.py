686eea31bb22aa1a1c3e0c54c79553860022dded
BarY7 <baryaacovi7@gmail.com>
2020-12-02 18:20:22 +0200

    finished

--- sgd.py
+++ sgd.py
@@ -1,10 +1,9 @@
 #################################
-# Your name:
+# Your name: Bar Yaacovi 208939009
 #################################
 
 # Please import and use stuff only from the packages numpy, sklearn, matplotlib
 
-import warnings
 import numpy as np
 import numpy.random
 from sklearn.datasets import fetch_openml
@@ -95,8 +94,13 @@ def SGD_ce(data, labels, eta_0, T):
     """
     Implements multi-class cross entropy loss using SGD.
     """
-    # TODO: Implement me
-    pass
+    number_of_samples = len(data)
+    w_l_vectors = np.zeros((10,784),dtype=np.longfloat)
+    for t in range(1, T+1):
+        random_i = random_sample_from_i(number_of_samples)
+        xi, yi = data[random_i], labels[random_i]
+        w_l_vectors = SGD_ce_step(w_l_vectors, eta_0, t, xi, yi)
+    return w_l_vectors
 
 #################################
 
@@ -122,15 +126,17 @@ def SGD_hinge_step(before_w, n0, t, C, xi, yi,):
 
     return after_w
 
-def SGD_ce_step(before_w, n0, t, xi, yi,):
+def SGD_ce_step(before_w_l_vectors, n0, t, xi, yi,):
     nt = n0
-    after_w = None
-    if(yi*np.inner(before_w, xi) < 1):
-        after_w = (1-nt)*before_w + nt*C*yi*xi
-    else:
-        after_w = np.multiply((1-nt), before_w)
-
-    return after_w
+    y_values = range(10)
+    softmax_probabilities = compute_softmax_vector(before_w_l_vectors,xi,y_values)
+    after_w_l_vectors = np.zeros((len(before_w_l_vectors), len(xi)))
+    for k in range(len(before_w_l_vectors)):
+        if (k == int(yi)):
+            after_w_l_vectors[k] = before_w_l_vectors[k] - nt * (softmax_probabilities[k]- 1)*xi
+        else:
+            after_w_l_vectors[k] = before_w_l_vectors[k] - nt * (softmax_probabilities[k])*xi
+    return after_w_l_vectors
 
 
 def cross_validation(validation_data, validation_labels, predictor):
@@ -145,12 +151,43 @@ def cross_validation(validation_data, validation_labels, predictor):
     return worked/len(validation_data)
 
 
+def compute_softmax_vector(predictor_l_vectors,xi,y_values):
+    sum = 0
+    probabilities = [0 for i in range(len(predictor_l_vectors))]
+    exp_values = np.array([np.inner(predictor_l_vectors[j], xi) for j in range(len(predictor_l_vectors))])
+    argmax_exp = exp_values.argmax()
+    normalize_power = np.inner(predictor_l_vectors[argmax_exp], xi)
+    for i in range(len(predictor_l_vectors)):
+        w_for_i = predictor_l_vectors[i]
+        sum = sum + (np.exp(np.inner(w_for_i, xi) - normalize_power))
+    for i in range(len(predictor_l_vectors)):
+        w_for_i = predictor_l_vectors[i]
+        nominator = np.exp(np.inner(w_for_i, xi) - normalize_power) 
+        probabilities[i] = nominator / sum
+    return probabilities
+
+def predict_ce_softmax(predictor_l_vectors, xi, y_values):
+    probabilities = compute_softmax_vector(predictor_l_vectors,xi,y_values)
+    return np.random.choice(y_values, p=probabilities)
+    
+
+
+def cross_validation_ce(validation_data, validation_labels, predictor_l_vectors, y_values):
+    worked = 0
+    for i in range(len(validation_data)):
+        x, y = validation_data[i], validation_labels[i]
+        predicted_y = predict_ce_softmax(predictor_l_vectors,x, y_values)
+        if(int(y) == predicted_y):
+            worked = worked + 1
+    return worked/len(validation_data)
+
+
 def q1_a():
     T = 1000
     C = 1
     n0_candidates = np.array([pow(10, i)
-                              for i in range(-5, 6)], dtype=np.longfloat)
-    n0_emp_average = np.array([0 for i in range(-5, 6)], dtype=np.longfloat)
+                              for i in range(-5, 4)], dtype=np.longfloat)
+    n0_emp_average = np.array([0 for i in range(-5, 4)], dtype=np.longfloat)
     number_of_runs = 10
     for i in range(number_of_runs):
         train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper_hinge()
@@ -163,17 +200,17 @@ def q1_a():
     print(n0_emp_average)
     line, = plt.plot(n0_candidates, n0_emp_average, label="eta0 to accuracy")
     plt.legend()
-    plt.xlim(pow(10, -5), 10**5)
+    plt.xlim(pow(10, -5), 10**3)
     plt.xscale('log')
-    plt.show()
+    #plt.show()
     return n0_candidates[n0_emp_average.argmin()]
 
 
 def q1_b():
     T = 1000
     n0 = 1
-    C_candidates = np.array([pow(10, i) for i in range(-5, 5)])
-    C_emp_average = np.array([0 for i in range(-5, 5)], dtype=np.float64)
+    C_candidates = np.array([pow(10, i) for i in range(-5, 4)])
+    C_emp_average = np.array([0 for i in range(-5, 4)], dtype=np.float64)
     number_of_runs = 10
     for i in range(number_of_runs):
         train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper_hinge()
@@ -187,27 +224,27 @@ def q1_b():
     line, = plt.plot(C_candidates, C_emp_average,
                      label="C candidates to accuracy")
     plt.legend()
-    plt.xlim(pow(10, -5), 10**5)
+    plt.xlim(pow(10, -5), 10**3)
     plt.xscale('log')
-    plt.show()
+    #plt.show()
     return C_candidates[C_emp_average.argmin()]
 
 
 def q1_c_d():
     T = 20000
     n0 = 1
-    C = 4
+    C = 10 ** -4
     train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper_hinge()
     predictor = SGD_hinge(train_data, train_labels, C, n0, T)
-    predictor_2d = np.reshape(predictor, (-1, 2))
+    predictor_2d = np.reshape(predictor, (28, 28))
     plt.imshow(predictor_2d)
-    accuracy = cross_validation(test_data, test_labels, predictor_2d)
+    #plt.show()
+    accuracy = cross_validation(test_data, test_labels, predictor)
     print(accuracy)
 
 
 def q2_a():
     T = 1000
-    C = 1
     n0_candidates = np.array([pow(10, i)
                               for i in range(-5, 6)], dtype=np.longfloat)
     n0_emp_average = np.array([0 for i in range(-5, 6)], dtype=np.longfloat)
@@ -218,15 +255,28 @@ def q2_a():
             predictor = SGD_ce(train_data, train_labels,
                                   n0_candidates[j], T)
             n0_emp_average[j] = n0_emp_average[j] + \
-                cross_validation(
-                    validation_data, validation_labels, predictor)/number_of_runs
+                cross_validation_ce(
+                    validation_data, validation_labels, predictor, range(10))/number_of_runs
     print(n0_emp_average)
     line, = plt.plot(n0_candidates, n0_emp_average, label="eta0 to accuracy")
     plt.legend()
     plt.xlim(pow(10, -5), 10**5)
     plt.xscale('log')
-    plt.show()
+    #plt.show()
     return n0_candidates[n0_emp_average.argmin()]
 
 
-q1_b()
+def q2_b_c():
+    T = 20000
+    n0 = 10 ** -3
+    train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper_ce()
+    accuracy = 0
+    predictor = SGD_ce(train_data, train_labels,n0, T)
+    for i in range(10):
+        predictor_2d = np.reshape(predictor[i], (28, 28))
+        plt.title("w{} image:".format(i))
+        plt.imshow(predictor_2d)
+        #plt.show()
+    accuracy =  + cross_validation_ce(validation_data, validation_labels, predictor, range(10))
+    print(accuracy)
+    return accuracy
